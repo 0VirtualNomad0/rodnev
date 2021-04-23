4aa11aff5a6d5997a1c620aee4ed74b5cd3abbc2
kushkd <kushkumardhawan@gmail.com>
2021-04-23 15:27:55 +0530

    -- Localization Updated   -- Main PAge   -- Form PAge   -- Login Page   -- Payment Proceed Page Done
    -- Fees Updated
    -- PCB Role Added
    -- PCB Role Functionality Added
    -- Home Page Updates
    -- Check Status (Completed)
    -- Generate Report Applicant (Beautification Completed)
     -- Vendor Image only PNG
     -- Mobile Application API (Done)
     -- App Optimization Completed
     -- Completed Encryption and Description API
    
    --TODO
     -- Mobile App

--- src/main/java/vendorapplication/apicontroller/API.java
+++ src/main/java/vendorapplication/apicontroller/API.java
@@ -14,6 +14,13 @@ import vendorapplication.security.EncryptDecrypt;
 import vendorapplication.services.UserApplicationService;
 import vendorapplication.utilities.Constants;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,14 +38,18 @@ public class API {
      */
     @RequestMapping(value = Constants.apiMethod, method = RequestMethod.POST, consumes = Constants.consumesProducesJson, produces = Constants.consumesProducesJson)
     @ResponseBody
-    public synchronized ResponseEntity<?> getApplicationDetails(@RequestBody String jsonDataApp ) {
+    public synchronized ResponseEntity<?> getApplicationDetails(@RequestBody String jsonDataApp ) throws UnsupportedEncodingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         logger.info("Json App Date:- "+ jsonDataApp);
         Map<String, Object> map = null;
+        EncryptDecrypt ED = new EncryptDecrypt();
+
         String appId = null, mobileNumber = null, userId = null ,  encrypted = null;
         if(jsonDataApp!=null && !jsonDataApp.isEmpty()){
             logger.info("API:: Data Not Empty");
+
             try {
-                JsonObject o = new JsonParser().parse(jsonDataApp).getAsJsonObject();
+                String decryptedData = ED.decrypt(jsonDataApp);
+                JsonObject o = new JsonParser().parse(decryptedData).getAsJsonObject();
                 System.out.println(o.toString());
                 logger.info("API:: Data is:- " + o);
                 appId = o.getAsJsonObject().get(Constants.application_id).getAsString();
@@ -49,7 +60,7 @@ public class API {
                 ObjectMapper Obj = new ObjectMapper();
                 String jsonStr = null;
                 jsonStr = Obj.writeValueAsString(data);
-                EncryptDecrypt ED = new EncryptDecrypt();
+
                 encrypted = ED.encrypt(jsonStr);
                 logger.info("API:: Json Object is:- " + jsonStr);
                 logger.info("API:: Encrypted Data is:- " + encrypted);
